0b3da37c252c012295486137a94a27254b4f8625
dabhiprakash <139905259+dabhiprakash@users.noreply.github.com>
2025-01-29 23:15:23 +0530

    quick commit

---
+++ about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    require_once "include/header_script.php";
+    ?>
+</head>
+
+<body>
+    <div class="page-wrapper">
+        <?php
+        require_once "include/header.php";
+        ?>
+
+        <main class="main">
+            <div class="page-header"
+                style="background-image: url('images/shop/page-header-back.jpg'); background-color: #3c63a4;">
+                <h3 class="page-subtitle"></h3>
+                <h1 class="page-title">About Us</h1>
+            </div>
+
+            <div class="page-content mb-10 pb-6">
+                <div class="container">
+                    <div class="row main-content-wrap gutter-lg">
+                        <div class="main-content ">
+                            <div class="row cols-12 cols-md-12 mt-9 cols-lg-12 product-wrapper">
+                                <p>
+Welcome to Shopercity! We're excited to have you join us in transforming your shopping experience. At Shopercity, we believe in community and supporting local businesses. Thank you for choosing Shopercity as your shopping companion! Our website, Shopercity.com, offers a wide variety of deals and discounts from nearby businesses across different categories such as Restaurants & Cafes, Grocery & Cosmetics, Movie Tickets Offers, Games & Amusement, Unisex Beauty Salon & Spa, Gyms & Yoga Classes, Dance & Aerobics, Travel & Tourism, Self Growth Course, Domestic & Global Jobs, and more. We're constantly updating our platform with new deals and discounts, and we encourage businesses and customers to share their experiences on social media. Our team works diligently to acquire great deals from the market, making your browsing hassle-free. Trust and transparency are crucial to us; we're registered as SHOPERCITY INFO SERVICES and adhere to all legal requirements, ensuring a safe online environment for both businesses and shoppers. Our user-friendly website makes it easy for you to find what you need and facilitates smooth transactions between buyers and sellers. We have different advertising and marketing plans to reach our target audience and customers, including referral programs where you can earn rewards for referrals. Our aim is to not only help businesses grow but also bring you the best savings for your daily needs. Join us on this journey of growth and discovery!
+</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <?php
+        require_once "include/footer.php";
+        ?>
+    </div>
+
+    <?php
+    require_once "include/mobile-menu.php";
+    require_once "include/footer_script.php";
+    ?>
+</body>
+
+</html>
---
+++ admin/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      
+    </div>
+    <strong>All Copyright &copy; 2021-<?php echo date('Y'); ?> <a href="javascript:void(0);">Shopercity</a>.</strong> All rights reserved.
+  </footer>
