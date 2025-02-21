2ff41e643e909c7c96009ea25cbca2d3dfab5c11
dabhiprakash <139905259+dabhiprakash@users.noreply.github.com>
2025-02-21 14:44:15 +0530

    sql file add

--- query.sql
+++ query.sql
@@ -5,3 +5,4 @@ ALTER TABLE `vendor` ADD `user_id` INT NULL DEFAULT NULL AFTER `id`;
 ALTER TABLE `vendor` CHANGE `status` `status` INT(10) NULL DEFAULT '0' COMMENT '0=pending\r\n1=reject\r\n2=approve\r\n3=delete';
 ALTER TABLE `vendor` ADD `reason` TEXT NULL DEFAULT NULL AFTER `status`;
 ALTER TABLE `vendor` CHANGE `city_id` `city_id` VARCHAR(255) NULL DEFAULT NULL;
+done
